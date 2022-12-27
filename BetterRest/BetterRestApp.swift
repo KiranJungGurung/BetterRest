c69957c2fc03b8ca83fb675adb5750cdc92736a8
KiranJungGurung <kirangurung394@gmail.com>
2022-12-27 06:05:59 +0545

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Kiran Gurung on 27/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
